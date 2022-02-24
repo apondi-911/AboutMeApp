3af2fdb5246b9b3a45091809e778a6d50a870173
apondi-911 <apondiashley2@gmail.com>
2022-02-24 13:58:38 +0300

    This was to show the use of scrollView

---
+++ app/src/main/java/com/example/aboutme/MainActivity.kt
@@ -0,0 +1,59 @@
+package com.example.aboutme
+
+import android.content.Context
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.view.inputmethod.InputMethodManager
+import android.widget.Button
+import android.widget.EditText
+import androidx.databinding.DataBindingUtil
+import com.example.aboutme.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private val myName:MyName= MyName("Apondi Ashley")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding= DataBindingUtil.setContentView(this,R.layout.activity_main)
+
+        binding.doneButton.setOnClickListener {
+            addNickname(it)
+        }
+        binding.nicknameText.setOnClickListener {
+            updateNickname(it)
+        }
+        binding.myName=myName
+
+    }
+
+    private fun addNickname(view: View){
+        binding.apply{
+            myName?.nickname = nicknameEdit.text.toString()
+            invalidateAll()
+            nicknameEdit.visibility = View.GONE
+            doneButton.visibility = View.GONE
+            nicknameText.visibility = View.VISIBLE
+        }
+
+
+        // Hide the keyboard.
+        val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        imm.hideSoftInputFromWindow(view.windowToken, 0)
+
+    }
+    private fun updateNickname(view:View){
+
+        val editText =findViewById<EditText>(R.id.nickname_edit)
+        val doneButton = findViewById<Button>(R.id.done_button)
+        editText.visibility = View.VISIBLE
+        doneButton.visibility = View.VISIBLE
+        view.visibility = View.GONE
+
+        // Hide the keyboard.
+        val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        imm.hideSoftInputFromWindow(view.windowToken, 0)
+    }
+
+}
